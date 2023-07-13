ef271a077b488675576164455d4365f74d3625fb
TomZanna <tommaso.zanotti@studenti.univr.it>
2023-07-13 11:06:12 +0200

    fix

--- src/main/java/it/univr/ipertesi/controller/HomePagePazienteController.java
+++ src/main/java/it/univr/ipertesi/controller/HomePagePazienteController.java
@@ -1,5 +1,6 @@
 package it.univr.ipertesi.controller;
 
+import it.univr.ipertesi.model.Therapy;
 import it.univr.ipertesi.utils.FXMLView;
 import it.univr.ipertesi.utils.StageManager;
 import it.univr.ipertesi.utils.UserSession;
@@ -25,7 +26,9 @@ public class HomePagePazienteController {
     }
 
     public void handlerInserimentoAssunzioneFarmaco() {
-        if (userSession.getPatient().getTherapy() == null) {
+        Therapy therapy = userSession.getPatient().getTherapy();
+
+        if (therapy == null || therapy.getPrescriptions().isEmpty()) {
             Alert notFoundPopup = new Alert(Alert.AlertType.INFORMATION);
             notFoundPopup.setHeaderText("Nessuna terapia!");
             notFoundPopup.setContentText("Puoi contattare il tuo medico scrivendo a " + userSession.getPatient().getDoctor().getEmail());
